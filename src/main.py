2a07acb82e8ae3951e8c5c6314b7cf850a3c9d6a
isagaidak <m.s.v.inkognito@gmail.com>
2023-09-09 20:45:56 +0300

    feat: timer rule added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import logging
 from os import environ
 
-from telegram.ext import ApplicationBuilder
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
 
 from handlers import HANDLERS
 
@@ -9,12 +10,28 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+
+async def timer_callback(context: CallbackContext):
+    await context.bot.send_message(context.job.chat_id, "Time's up!")
+
+
+async def timer(update: Update, context: CallbackContext):
+    minutes = 1
+    MAX_MINUTES = 1440
+    if context.args and context.args[0].isdigit() and minutes <= MAX_MINUTES:
+        minutes = int(context.args[0])
+    await update.message.reply_text(f'Timer started for {minutes} minutes.')
+
+    application.job_queue.run_once(timer_callback, minutes * 60, chat_id=update.message.chat_id)
+
 
 def main():
     if not BOT_TOKEN:
         raise Exception('Missing bot token! Add the token to environment variable BOT_TOKEN')
 
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler('timer', timer))
 
     for handler in HANDLERS:
         application.add_handler(handler)
