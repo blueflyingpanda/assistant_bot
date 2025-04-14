5be701cb26f949b218331170bec9fb2b34f850cf
OrliogloDenis <orlioglodenis24@gmail.com>
2025-04-14 23:53:55 +0300

    Fix7

--- src/conf.py
+++ src/conf.py
@@ -1,9 +1,9 @@
 from os import environ
 
-BOT_TOKEN = environ.get('BOT_TOKEN','7437435023:AAHUBkyGI1ugHd5tHSSxKUsyP4m-uJF8pY8')
+BOT_TOKEN = environ.get('BOT_TOKEN')
 
-DB_USER = environ.get('DB_USER', 'postgres')
-DB_PASS = environ.get('DB_PASS', 'root')
+DB_USER = environ.get('DB_USER', 'bot')
+DB_PASS = environ.get('DB_PASS', 'bot')
 DB_HOST = environ.get('DB_HOST', 'localhost')
 DB_PORT = environ.get('DB_PORT', '5432')
-DB_NAME = environ.get('DB_NAME', 'postgres')
+DB_NAME = environ.get('DB_NAME', 'bot')
